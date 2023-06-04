ceea9a179444645478473321dba40ee981c582e1
maxinunezz <maxicbapl@gmail.com>
2023-06-04 20:47:11 -0300

    act

--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -1,12 +1,42 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+const { v4: uuidv4 } = require("uuid");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: () => uuidv4(),
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      platforms: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      releaseDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
+    { timestapms: false }
+  );
 };
